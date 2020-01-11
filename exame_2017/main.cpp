f0e97051e03cbdbbbcc638b04dec20690ef944ea
Beatriz Mendes <45185351+biam05@users.noreply.github.com>
2020-01-11 11:50:43 +0000

    Add files via upload

--- exame_2017/main.cpp
+++ exame_2017/main.cpp
@@ -3,100 +3,52 @@
 
 using namespace std;
 
-double f1(double x, double y){ return pow(x, 2) - y - 1.2;}
-double f2(double x, double y){ return -x + pow(y, 2) - 1.0;}
-double f1x(double x, double y) { return 2 * x; }
-double f1y(double x, double y) { return -1; }
-double f2x(double x, double y) { return -1; }
-double f2y(double x, double y) { return 2*y; }
+double f(double x){	return pow(x - 1, 2) + pow(x, 4);}
 
-double f(double x) { return 1 * pow(x, 7) + 0.5*x - 0.5; }
+double y(double x) { return sqrt(1 + pow(2.5*exp(2.5*x), 2)); }
 
-double fy(double t, double y, double z) {return z;}
-double fz(double t, double y, double z) {return 0.5 + pow(t,2) + t*fy(t,y,z);}
+double f1(double x) { return exp(x) - x - 5; }
+double df1x(double x) { return exp(x) - 1; }
+double f2(double x) { return exp(x) - 5; }
+double f3(double x) { return log(5 + x); }
 
-double f6(double x) { return sqrt(1 + pow(1.5*exp(1.5*x), 2));}
+double dCdt(double C, double T) { return -exp(-0.5 / (T + 273))*C; }
+double dTdt(double C, double T) { return 30 * exp(-0.5 / (T + 273)) * C - 0.5 * (T - 20); }
 
-void mnewton(double xn, double yn)
+void aurea(double x1, double x2, double precision)
 {
-	for (int i = 0; i < 3; i++)
-	{
-		cout << "xn: " << xn << "\t yn: " << yn << endl;
-		double jacobian = f1x(xn, yn)* f2y(xn, yn) - f1y(xn, yn) * f2x(xn, yn);
-		double hn = (f1(xn, yn)*f2y(xn, yn) - f1y(xn, yn)*f2(xn, yn)) / jacobian;
-		double kn = (f1x(xn, yn)*f2(xn, yn) - f1(xn, yn)*f2x(xn, yn)) / jacobian;
-		xn -= hn;
-		yn -= kn;		
-	}
-}
+	const double B = (sqrt(5) - 1) / 2;
+	const double A = B * B;
+	double x3 = x1 + A * (x2 - x1);
+	double x4 = x1 + B * (x2 - x1);
 
-void mcorda(double a, double b)
-{
-	for (int i = 0; i < 4; i++)
+	while (abs(x1 - x4) > precision && abs(x3 - x2) > precision)
 	{
-		double rr = (a*f(b) - b * f(a)) / (f(b) - f(a));
-
-		cout << "iteracao: " << i + 1 << "\ta (xe): " << a << "\tb (xd) " << b << "\tx (xn): " << rr << "\tf(x): " << f(rr) << endl;
-		if (f(b) - f(a) == 0)
-			break;
-		
-		if (f(a) * f(rr) < 0)
-			b = rr;
+		if (f(x3) < f(x4))
+		{
+			x2 = x4;
+			x4 = x3;
+			x3 = x1 + B * (x4 - x1);
+		}
 		else
-			a = rr;
-		
+		{
+			x1 = x3;
+			x3 = x4;
+			x4 = x3 + B * (x2 - x3);
+		}
 	}
-}
 
-void meuler_2a(double t, double y, double z, double h)
-{
-	double t1, y1, z1;
-	for (int i = 0; i < 3 ; i++)
-	{
-		cout << "iteracao: " << i << "\tt: " << t << "\ty: " << y << endl;
-		t1 = t + h;
-		y1 = y + z * h;
-		z1 = fz(t, y, z)*h + z;
-		t = t1;
-		y = y1;
-		z = z1;
-	}
-}
+	cout << "x1: " << x1 << "\tx2: " << x2 << "\tx3: " << x3 << "\tx4: " << x4 << endl;
 
-void mrk4_2a(double t, double y, double z, double h)
-{
-	double y1, y2, y3, y4, z1, z2, z3, z4, zn, yn, zn1, yn1;
-	for (int i = 0; i < 3; i++)
-	{
-		cout << "iteracao: " << i << "\tt: " << t << "\ty: " << y << endl;
-
-		z1 = h * fz(t, y, z);
-		y1 = h * fy(t, y, z);
-		z2 = h * fz(t + h / 2, y + y1 / 2, z + z1 / 2);
-		y2 = h * fy(t + h / 2, y + y1 / 2, z + z1 / 2);
-		z3 = h * fz(t + h / 2, y + y2 / 2, z + z2 / 2);
-		y3 = h * fy(t + h / 2, y + y2 / 2, z + z2 / 2);
-		z4 = h * fz(t + h, y + y3, z + z3);
-		y4 = h * fy(t + h, y + y3, z + z3);
-
-		zn = z1 / 6.0 + z2 / 3.0 + z3 / 3.0 + z4 / 6.0;
-		yn = y1 / 6.0 + y2 / 3.0 + y3 / 3.0 + y4 / 6.0;
-
-		yn1 = y + yn;
-		zn1 = z + zn;
-		t += h;
-		y = yn1;
-		z = zn1;
-	}
 }
 
 double mtrapezios(double a, double b, double h)
 {
-	double n = (b - a) / h; 
-	double s = f6(a) + f6(b);
+	double s = y(a) + y(b);
+	double n = (b - a) / h;
 	for (int i = 1; i < n; i++)
 	{
-		s += 2 * f6(a + i * h);
+		s += 2 * y(a + i * h);
 	}
 	s *= h / 2;
 	return s;
@@ -104,48 +56,141 @@ double mtrapezios(double a, double b, double h)
 
 double msimpson(double a, double b, double h)
 {
+	double s = y(a) + y(b);
 	double n = (b - a) / h;
-	double s = f6(a) + f6(b);
 	for (int i = 1; i < n; i++)
 	{
 		if(i % 2 == 0)
-			s += 2 * f6(a + i * h);
+			s += 2 * y(a + i * h);
 		else
-			s += 4 * f6(a + i * h);
+			s += 4 * y(a + i * h);
 	}
 	s *= h / 3;
 	return s;
 }
 
+void mnewton(double x)
+{
+	int i = 0;
+	double x1 = 100;
+	while(abs(x1 - x) > 0.00001)
+	{
+		x1 = x;
+		x = x1 - f1(x1) / df1x(x1);
+		i++;
+	}
+	cout <<	"i: " << i << "\tx: " << x << endl;
+}
+
+void mpicardpeano(double x)
+{
+	int i = 0;
+	double x1 = 100;
+	while (abs(x1 - x) > 0.00001)
+	{
+		x1 = x;
+		x = f3(x1);
+		i++;
+	}
+	cout << "i: " << i << "\tx: " << x << endl;
+}
+
+double meuler(double h, double C, double T)
+{
+	double t = 0, T1, C1, i = 0, tprev;
+	while( t <= 0.5)
+	{
+		cout << "iteracao: " << i << "\tt: " << t << "\tC: " << C << "\tT: " << T << endl;
+		tprev = T;
+		t += h;
+		T1 = T + dTdt(C, T) * h;
+		C1 = C + dCdt(C, T) * h;
+		T = T1;
+		C = C1;
+		i++;
+	}
+	return tprev;
+}
+
+double mrk4(double h, double C, double T)
+{
+	double o1c, o2c, o3c, o4c, o1t, o2t, o3t, o4t, t = 0;
+	for (int i = 0; i < 3; i++)
+	{
+		cout << "iteracao: " << i << "\tt: " << t << "\tC: " << C << "\tT: " << T << endl;
+		t += h;
+		o1c = h * dCdt(C, T);
+		o1t = h * dTdt(C, T);
+		o2c = h * dCdt(C + o1c / 2, T + o1t / 2);
+		o2t = h * dTdt(C + o1c / 2, T + o1t / 2);
+		o3c = h * dCdt(C + o2c / 2, T + o2t / 2);
+		o3t = h * dTdt(C + o2c / 2, T + o2t / 2);
+		o4c = h * dCdt(C + o3c, T + o3t);
+		o4t = h * dTdt(C + o3c, T + o3t);
+		C += (o1c / 6.0 + o2c / 3.0 + o3c / 3.0 + o4c / 6.0);
+		T += (o1t / 6.0 + o2t / 3.0 + o3t / 3.0 + o4t / 6.0);
+	}
+	return T;
+}
+
 int main()
 {
-	cout << "\t\tMETODO DE NEWTON (SISTEMAS) - EX 2" << endl << endl;
-	mnewton(1, 1);
-	cout << "\n\t\tMETODO DA CORDA - EX4" << endl << endl;
-	mcorda(0, 0.8);
-	cout << "\n\t\tMETODOS DE INTEGRACAO - EX5" << endl << endl;
-	cout << "\t\t\tMETODO DE EULER" << endl << endl;
-	meuler_2a(0, 0, 1, 0.25);
-	cout << "\n\t\t\tMETODO DE RK4" << endl << endl;
-	mrk4_2a(0, 0, 1, 0.25);
-	cout << "\n\t\tQUADRATURA - EX6" << endl << endl;
+	cout << "\t\tMETODO DA SECCAO AUREA - EX1" << endl << endl;
+	aurea(1.0, 3.0, 0.001);
+
+
+	cout << "\n\t\tQUADRATURA - EX2" << endl << endl;
+
 	cout << "\t\t\tMETODO DOS TRAPEZIOS" << endl << endl;
-	double it1 = mtrapezios(0, 2.0, 0.5);
-	double it2 = mtrapezios(0, 2.0, 0.5 / 2.0);
-	double it3 = mtrapezios(0, 2.0, 0.5 / 4.0);
+
+	double it1 = mtrapezios(0, 1.0, 0.125);
+	double it2 = mtrapezios(0, 1.0, 0.125 / 2.0);
+	double it3 = mtrapezios(0, 1.0, 0.125 / 4.0);
 	cout << "I: " << it1 << "\tI': " << it2 << "\tI'': " << it3 << endl;
 	double qct = (it2 - it1) / (it3 - it2);
 	cout << "Qc: " << qct << endl;
 	double et = (it3 - it2) / 3;
 	cout << "E: " << et << endl;
+
 	cout << "\n\t\t\tMETODO DE SIMPSON" << endl << endl;
-	double is1 = msimpson(0, 2.0, 0.5);
-	double is2 = msimpson(0, 2.0, 0.5 / 2.0);
-	double is3 = msimpson(0, 2.0, 0.5 / 4.0);
+	double is1 = msimpson(0, 1.0, 0.125);
+	double is2 = msimpson(0, 1.0, 0.125 / 2.0);
+	double is3 = msimpson(0, 1.0, 0.125 / 4.0);
 	cout << "I: " << is1 << "\tI': " << is2 << "\tI'': " << is3 << endl;
 	double qcs = (is2 - is1) / (is3 - is2);
 	cout << "Qc: " << qcs << endl;
 	double es = (is3 - is2) / 15;
 	cout << "E: " << es << endl;
+
+
+	cout << "\n\t\tPICARD-PEANO VS NEWTON - EX3" << endl << endl;
+
+	cout << "\t\t\tMETODO DE NEWTON" << endl << endl;
+	mnewton(2);
+
+	cout << "\n\t\t\tMETODO DE PICARD PEANO" << endl << endl;
+	mpicardpeano(2);
+
+
+	cout << "\n\t\tMETODO DE EULER vs METODO RK4 - EX4" << endl << endl;
+
+	cout << "\t\t\tMETODO DE EULER" << endl << endl;
+	double Teuler = meuler(0.25, 2.5, 25);
+
+	cout << "\n\t\t\tMETODO DE RK4" << endl << endl;
+	double Trk41 = mrk4(0.25, 2.5, 25);
+
+	cout << "\n\t\t\tQC E ERRO NO METODO DE EULER (C)" << endl << endl;
+	double Teuler1 = meuler(0.25, 2.5, 25.0);
+	cout << endl;
+	double Teuler2 = meuler(0.25 / 2.0, 2.5, 25.0);
+	cout << endl;
+	double Teuler3 = meuler(0.25 / 4.0, 2.5, 25.0);
+	cout << endl << "T': " << Teuler2 << "\tT'': " << Teuler3 << endl;
+	double QCTeuler = (Teuler2 - Teuler1) / (Teuler3 - Teuler2);
+	cout << "Qc: " << QCTeuler << endl;
+	double ETeuler = (Teuler3 - Teuler2);
+	cout << "E: " << ETeuler << endl;
+
 	return 0;
 }
